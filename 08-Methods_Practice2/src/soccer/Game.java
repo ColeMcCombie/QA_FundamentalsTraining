1e2bda8874a6b3b004f600caa8c190a482cf2ff1
coleycombie <nicole.mccombie@atos.net>
2015-11-19 13:33:19 +0000

    All projects for QA Week 1 up till Thursday 19th November

---
+++ 08-Methods_Practice2/src/soccer/Game.java
@@ -0,0 +1,50 @@
+/*
+
+* To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package soccer;
+
+import static soccer.League.createGames;
+import static soccer.League.createTeams;
+import utility.GameUtils;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Game {
+    
+    public Team homeTeam;
+    public Team awayTeam;
+    public Goal[] goals;
+    
+    /* Practice 8-2. Add playGame() method here */
+    
+    public void playGame()
+    {
+        int numberOfGoals = (int)(Math.random() * 7);
+        Goal[] theGoals = new Goal[numberOfGoals];
+        this.goals = theGoals;
+        GameUtils.addGameGoals(this); 
+        
+    }
+    public String getDescription() {
+        StringBuilder returnString = new StringBuilder();
+        for (Goal currGoal: this.goals) {
+            returnString.append("Goal scored after "
+            + currGoal.theTime + " mins by "
+            + currGoal.thePlayer.playerName + " of "
+            + currGoal.theTeam.teamName +
+              "\n");
+        }
+        return returnString.toString();
+    }
+    
+    
+    /* Practice 8-2. Add getDescription() method here */
+    
+      
+}
